0714a0b0dd139de5bd8e11b041a1f61df44c5d31
Nehcnud <nenaddjukin@gmail.com>
2023-07-22 15:18:14 +0200

    inital start with login

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+  Card,
+} from "react-bootstrap";
+import Login from "./components/Login";
+
+import { useState, useEffect } from "react";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
